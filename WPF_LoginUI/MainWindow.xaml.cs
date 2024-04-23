1f2ac5e3bf8be4fedf4f778de454af527d728302
huliawsl <3150399699@qq.com>
2024-04-23 22:22:54 +0800

    实现基本登录，实现双向绑定

--- WPF_LoginUI/MainWindow.xaml.cs
+++ WPF_LoginUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,64 @@ namespace WPF_LoginUI
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window,INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _UserName;
+
+        public string UserName
+        {
+            get { return _UserName; }
+            set { 
+                _UserName = value;
+                RaisePropertyChanged("UserName");
+            }
+        }
+
+        private string _Password;
+
+        public string Password
+        {
+            get { return _Password; }
+            set
+            {
+                _Password = value;
+                RaisePropertyChanged("Password");
+            }
+        }
+
+        private void Button_click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (UserName == "123" && Password == "2")
+            {
+                MessageBox.Show("登录成功");
+                Index index = new Index();
+                index.Show();
+                this.Close();
+            }
+            else
+            {
+                SystemSounds.Asterisk.Play();
+                MessageBox.Show("输入的用户名和密码不正确");
+                
+            }
+            UserName = "";
+            Password = "";
         }
     }
 }
