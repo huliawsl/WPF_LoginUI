31cedc52262c7041eddd4c62da6aaecbe300841a
huliawsl <3150399699@qq.com>
2024-04-24 22:06:53 +0800

    vm

---
+++ WPF_LoginUI/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_LoginUI
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private LoginModel _LoginM = new LoginModel();
+
+        public string UserName
+        {
+            get { return _LoginM.UserName; }
+            set { 
+                _LoginM.UserName = value;
+                RaisePropertyChanged(nameof(UserName));
+            }
+        }        
+        public string Password
+        {
+            get { return _LoginM.Password; }
+            set { 
+                _LoginM.Password = value;
+                RaisePropertyChanged(nameof(Password));
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+               handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
